7fd57d6ded79949365755f3209c78cb5ef613ba1
Cedric Bouhadra <cbouhadr@c1r2s2.42lausanne.ch>
2024-12-17 15:16:09 +0100

    so_long

--- Makefile
+++ Makefile
@@ -1,17 +1,29 @@
 CC = gcc 
-GFLAGS= -Werror -Wall- Wextra
+GFLAGS= -Werror -Wall -Wextra
 SRCS = srcs/main.c 
 NAME= so_long
 
+OBJS = $(SRCS:.c =.o)
 
+all: $(NAME)
 
-$(NAME): $(OBJ)
-	$(CC) $(OBJ) ${SRCS} -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz -o $(NAME)
+$(NAME): $(OBJS)
+	$(CC) $(GFLAGS) $(OBJS) -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz -o $(NAME)
 
-%.o: %.c
-	$(CC) -Wall -Wextra -Werror -I/usr/include -Imlx_linux -O3 -c $< -o $@
+#%.o: %.c
+#	$(CC) -Wall -Wextra -Werror -I/usr/include -Imlx_linux -O3 -c $< -o $@
 
 clean:
 	rm -f srcs/*.o
+
 fclean: clean
 	rm -f ${NAME}
+
+re: clean fclean ${NAME}
+
+gdev:
+	git add .
+	git commit -m ${NAME}
+	git push
+
+.PHONY: re all clean fclean so_long
--- srcs/main.c
+++ srcs/main.c
@@ -1,13 +1,56 @@
-#include "../mlx_linux/mlx.h"
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbouhadr <cbouhadr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 14:40:59 by cbouhadr          #+#    #+#             */
+/*   Updated: 2024/12/17 14:55:35 by cbouhadr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+
+typedef struct s_data
+{
+    void    *img;
+    char    *addr;
+    int     bit_per_pixel;
+    int     line_length;
+    int     endian;
+
+
+
+} t_data;
+
+
+void ft_put_px(t_data *data, int x, int y, int color)
+{
+    char *dst;
+
+
+    dst = data->addr + (y * data->line_length + x * (data->bit_per_pixel / 8));
+    *(unsigned int *)dst = color;
+}
 
 
 int	main(void)
 {
     void	*mlx;
-    
+    void    *new_w;
+    t_data  img;
+
+
     mlx = mlx_init();
-    printf("%p\n", mlx);
+    new_w = mlx_new_window(mlx, 1920, 1080, "hello");
+    img.img = mlx_new_image(mlx, 1920, 1080);
+    img.addr = mlx_get_data_addr(img.img, &img.bit_per_pixel, &img.line_length, &img.endian);
+    ft_put_px(&img, 5, 5, 0x00FF0000);
+    mlx_put_image_to_window(mlx, new_w, img.img, 0, 0);
+
+    mlx_loop(mlx);
+
     return(0);
 }
 
